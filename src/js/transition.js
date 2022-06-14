2ee5f21a12a20b03f41661cd1c70a321e0b3eb3d
zoro <jagbetuyi001@gmail.com>
2022-06-14 22:14:23 +0100

    transition

--- src/js/transition.js
+++ src/js/transition.js
@@ -3,14 +3,28 @@ const transition = () => {
   const side = document.querySelector("#side");
   const input = document.querySelector("input");
   const title = document.querySelector("h1");
+  const moveBack = document.querySelector("#move-back");
+  const main = document.querySelector("#main");
 
   button.addEventListener("click", () => {
     side.style.width = "20vw";
-    title.style.opacity = "0";
+    title.style.opacity = "0.2";
+    input.disabled = true;
+    button.disabled = true;
+    side.style.opacity = "0.5";
+    moveBack.style.opacity = "1";
+    moveBack.style.cursor = "pointer";
+    main.style.backgroundColor = "navajowhite";
   });
-  //   side.addEventListener("click", () => {
-  //     side.style.width = "200vw";
-  //   });
+  moveBack.addEventListener("click", () => {
+    side.style.width = "200vw";
+    title.style.opacity = "1";
+    side.style.opacity = "1";
+    moveBack.style.opacity = "0.2";
+    input.disabled = false;
+    button.disabled = false;
+    main.style.backgroundColor = "antiquewhite";
+  });
 };
 
 export { transition };
